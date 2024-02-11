dab7640e3ba633e81ff1cf3eaa93ff2eafd6d47a
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-02-11 15:05:34 +0100

    import and export test files to game.js

---
+++ scripts/game.js
@@ -0,0 +1,7 @@
+let game = {
+    score: 0,
+}
+
+
+// EXPORT module to game.test.js must be at the END
+module.exports = { game };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,6 +2,9 @@
  * @jest-environment jsdom
  */
 
+//IMPORT game.js
+const { game } = require("../game");
+
 // SAME CODE for every HTML file I want to load to the DOM
 beforeAll(() => {
     let fs = require("fs"); //part of node's default standard library
@@ -9,4 +12,10 @@ beforeAll(() => {
     document.open();
     document.write(fileContent);
     document.close();
+})
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    })
 })
