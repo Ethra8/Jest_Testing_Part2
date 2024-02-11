8dbe45217b2977d28873454e0e58439e5392cb70
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-02-11 14:57:39 +0100

    node modules

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,12 @@
 /**
  * @jest-environment jsdom
  */
+
+// SAME CODE for every HTML file I want to load to the DOM
+beforeAll(() => {
+    let fs = require("fs"); //part of node's default standard library
+    let fileContent = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContent);
+    document.close();
+})
